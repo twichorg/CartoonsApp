5d719e9d1037bffc2948f6259537646f39da33ec
can aktürk <52425556+twichorg@users.noreply.github.com>
2020-10-10 11:10:27 +0300

    initial commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './card.style.css';
+
+export  const Card = (props) => (
+<div className='card-container'>
+<img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set&size=180x180`}></img>
+<h2> {props.monster.name} </h2>
+</div>
+)
